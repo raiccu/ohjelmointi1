35d2da861463287b36280582de95b240d88befdc
Raine Mätäsaho <raine.matasaho@gmail.com>
2023-08-24 10:39:59 +0300

    added code

--- Maalaus.java
+++ Maalaus.java
@@ -1,8 +1,12 @@
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Maalaus {
     public static void main(String[] args){
-        String maalaus = "maalaus";
+        Scanner lukija = new Scanner(System.in);
+        System.out.println("Anna huoneen leveys, pituus ja korkeus: ");
+        
+        
     }
 }
 
