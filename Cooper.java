ffd341c6981175f35af7fa764c46831ec9ae4eb6
Raine Mätäsaho <raine.matasaho@gmail.com>
2023-08-24 13:53:09 +0300

    finished new exercise

---
+++ Cooper.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Cooper {
+    public static void main(String[] args) {
+        Scanner cooper = new Scanner(System.in);
+        System.out.println("Anna juostu matka: ");
+        int matka = cooper.nextInt();
+
+        int kierrokset = matka / 400;
+
+        System.out.println("Kokonaisia 400 metrin kierroksia oli " + kierrokset);
+
+    }
+}
