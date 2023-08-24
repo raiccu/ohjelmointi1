2bce015605f3c3eed1270445e2e8a2ce5cbc0603
Raine Mätäsaho <raine.matasaho@gmail.com>
2023-08-24 15:10:55 +0300

    finished this exercise in one go

---
+++ Juhlat.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Juhlat {
+    public static void main(String[] args){
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Anna juhliin tulevien aikuisten määrä: ");
+
+        int aikuiset = lukija.nextInt();
+        int lasiaPerPullo = 7;
+        int tarvittavatPullot = (int)Math.ceil((double)aikuiset / lasiaPerPullo);
+
+        int ylimääräiset = (tarvittavatPullot * lasiaPerPullo) - aikuiset;
+        
+
+        System.out.println("Pulloja tarvitaan " + tarvittavatPullot + " kappaletta");
+
+        System.out.println("Viimeisestä pullosta jää " + ylimääräiset + " lasia");
+    }
+}
