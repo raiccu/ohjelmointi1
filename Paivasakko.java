afcac22f9c46ec1a5fa30d4c89db22a2c866e9f1
Raine Mätäsaho <raine.matasaho@gmail.com>
2023-08-25 09:05:30 +0300

    completed one exercise

---
+++ Paivasakko.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Paivasakko {
+    public static void main(String[] args){
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Anna nettotulot: ");
+        int nettotulo = lukija.nextInt();
+
+        int paivasakko = (nettotulo - 255) / 60;
+
+        System.out.println("Nettotuloilla " + nettotulo + "päiväsakko on " + paivasakko + "euroa");
+    }
+}
